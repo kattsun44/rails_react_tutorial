d04ef6981dd901b1a6647748d3362a842623151d
kattsun44 <kattsun4416@gmail.com>
2025-02-24 12:08:16 +0900

    fix: Task.tsx を import
    ref: https://zenn.dev/prune/books/0d7d6e3c5f0496/viewer/f0c70e

--- todo-web/src/App.tsx
+++ todo-web/src/App.tsx
@@ -1,8 +1,14 @@
 import './App.css'
+import Task from "./component/Task"
 
 function App() {
   return (
-    <h1>これはテストです</h1>
+    <>
+      <h1>タスク一覧</h1>
+      <Task name="買い物" />
+      <Task name="ランニング" />
+      <Task name="プログラミングの勉強" />
+    </>
   )
 }
 
