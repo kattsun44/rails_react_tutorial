6758e181682e1e4ba9e60917afeade83560cb8a5
kattsun44 <kattsun4416@gmail.com>
2025-02-24 13:16:56 +0900

    feat: ChakuraUIからコンポーネントをimport
    refs:
    - https://zenn.dev/prune/books/0d7d6e3c5f0496/viewer/1835f2#taskコンポーネントにchakurauiをあてる
    - https://chakra-ui.com/docs/components/checkbox

--- todo-web/src/component/Task.tsx
+++ todo-web/src/component/Task.tsx
@@ -1,10 +1,17 @@
+import { Checkbox } from "../components/ui/checkbox"
+import { Box, Text } from "@chakra-ui/react"
+
 type Props = {
   name: string
 }
 
 const Task: React.FC<Props> = (props) => {
   return (
-    <li>{props.name}</li>
+    <Box mb="16px">
+      <Checkbox colorScheme="blue" size="lg">
+        <Text>{props.name}</Text>
+      </Checkbox>
+    </Box>
   )
 }
 
